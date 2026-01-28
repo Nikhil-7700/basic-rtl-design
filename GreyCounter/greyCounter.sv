2d15b397a5c5513a875fc86eee191041ad9a4a66
Padmanabha Nikhil Bhimavarapu <63917222+Nikhil-7700@users.noreply.github.com>
2026-01-28 11:23:33 -0700

    Update greyCounter.sv

--- GreyCounter/greyCounter.sv
+++ GreyCounter/greyCounter.sv
@@ -1,4 +1,5 @@
-module greyCounter(
+// Method 1
+/*module greyCounter(
   input clk, rst,
   output [3:0] greyCnt
 );
@@ -15,6 +16,32 @@ module greyCounter(
                     counter[3] ^ counter[2],
                     counter[2] ^ counter[1],
                     counter[1] ^ counter[0] };
+  
+  
+  
+
+endmodule*/
+
+
+// Method 2
+module greyCounter(
+  input clk, rst,
+  output reg [2:0] Qout
+);
+  
+  //reg [2:0] Qout_r;
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) Qout <= 0;
+    
+    else begin
+      Qout[2] <= ( Qout[2] & Qout[0] ) | ( Qout[1] & ~Qout[0] );
+      Qout[1] <= ( ~Qout[2] & Qout[0] ) | ( Qout[1] & ~Qout[0] );
+      Qout[0] <= ~(Qout[2] ^ Qout[1]);
+      
+    end
+    
+  end
   
   
   
